f03c1ce4c19c86c1171d79c7784c64fe5a5a9d2b
Simokod <44751590+Simokod@users.noreply.github.com>
2018-12-12 21:09:15 +0200

    Delete AcquireVehicleEvent.java

--- AcquireVehicleEvent.java
+++
@@ -1,7 +0,0 @@
-package bgu.spl.mics.application.messages;
-
-import bgu.spl.mics.Event;
-import bgu.spl.mics.application.passiveObjects.DeliveryVehicle;
-
-public class AcquireVehicleEvent implements Event<DeliveryVehicle> {
-}
