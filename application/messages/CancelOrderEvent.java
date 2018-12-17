1a15d30582a3de24f2e29b6d9feb8e8d4e340242
Simokod <44751590+Simokod@users.noreply.github.com>
2018-12-17 11:37:54 +0200

    Delete CancelOrderEvent.java

--- application/messages/CancelOrderEvent.java
+++
@@ -1,6 +0,0 @@
-package bgu.spl.mics.application.messages;
-
-import bgu.spl.mics.Event;
-            // sent by APIService, handled by           // TODO: check if necessary
-public class CancelOrderEvent implements Event<Boolean> {
-}
